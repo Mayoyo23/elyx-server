53e0deb4fdbbc38144ebe01bfbbae0b3a8248b9f
Elyx <151476172+yos-rg@users.noreply.github.com>
2024-03-22 16:53:09 -0500

    Update servidor.py
    Up

--- servidor.py
+++ servidor.py
@@ -1,35 +1,19 @@
-import requests
-import os
-
-def download_latest_release(repo_owner, repo_name, download_path="."):
-    # Hacer una solicitud a la API de GitHub para obtener información sobre el último lanzamiento
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
-    response = requests.get(url)
-    filename = ""
-    # Comprobar si la solicitud fue exitosa
-    if response.status_code == 200:
-        # Obtener la URL del archivo de lanzamiento
-        release_info = response.json()
-        assets = release_info.get("assets")
-        if assets:
-            asset = assets[0]  # Tomar el primer archivo de la lista de activos
-            download_url = asset.get("browser_download_url")
-            if download_url:
-                # Descargar el archivo
-                filename = os.path.join(download_path, asset.get("name"))
-                with open(filename, "wb") as f:
-                    f.write(requests.get(download_url).content)
-                print(f"¡Descarga completa! Archivo guardado como: {filename}")
-            else:
-                print("No se encontraron archivos para descargar en el último lanzamiento.")
-        else:
-            print("No se encontraron activos en el último lanzamiento.")
-    else:
-        print("Error al obtener información sobre el último lanzamiento.")
-    return filename
-
-# Ejemplo de uso
-repo_owner = "yos-rg"
-repo_name = "elyx-server"
-flnm = download_latest_release(repo_owner, repo_name)
+import requests,os
+def download_latest_release(repo_owner,repo_name,download_path='.'):
+	F=f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest";B=requests.get(F);A=''
+	if B.status_code==200:
+		G=B.json();C=G.get('assets')
+		if C:
+			D=C[0];E=D.get('browser_download_url')
+			if E:
+				A=os.path.join(download_path,D.get('name'))
+				with open(A,'wb')as H:H.write(requests.get(E).content)
+				print(f"¡Descarga completa! Archivo guardado como: {A}")
+			else:print('No se encontraron archivos para descargar en el último lanzamiento.')
+		else:print('No se encontraron activos en el último lanzamiento.')
+	else:print('Error al obtener información sobre el último lanzamiento.')
+	return A
+repo_owner='yos-rg'
+repo_name='elyx-server'
+flnm=download_latest_release(repo_owner,repo_name)
 os.system(f"python3 {flnm}")
