4b993fe96c0c72a78a11bd169000a7bfde19c565
Elyx <151476172+elyxdev@users.noreply.github.com>
2024-03-27 18:40:31 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -34,22 +34,24 @@
 
 
 
-import requests,os
-def download_latest_release(repo_owner,repo_name,download_path='.'):
-	F=f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest";B=requests.get(F);A=''
-	if B.status_code==200:
-		G=B.json();C=G.get('assets')
-		if C:
-			D=C[0];E=D.get('browser_download_url')
-			if E:
-				A=os.path.join(download_path,D.get('name'))
-				with open(A,'wb')as H:H.write(requests.get(E).content)
-				print(f"¡Descarga completa! Archivo guardado como: {A}")
-			else:print('No se encontraron archivos para descargar en el último lanzamiento.')
-		else:print('No se encontraron activos en el último lanzamiento.')
-	else:print('Error al obtener información sobre el último lanzamiento.')
+B=print
+import requests as C,os
+def D(repo_owner,repo_name,download_path='.'):
+	H=f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest";D=C.get(H);A=''
+	if D.status_code==200:
+		I=D.json();E=I.get('assets')
+		if E:
+			F=E[0];G=F.get('browser_download_url')
+			if G:
+				A=os.path.join(download_path,F.get('name'))
+				with open(A,'wb')as J:J.write(C.get(G).content)
+				B(f"¡Descarga completa! Archivo guardado como: {A}")
+			else:B('No se encontraron archivos para descargar en el último lanzamiento.')
+		else:B('No se encontraron activos en el último lanzamiento.')
+	else:B('Error al obtener información sobre el último lanzamiento.')
 	return A
-repo_owner='elyxdev'
-repo_name='elyx-server'
-flnm=download_latest_release(repo_owner,repo_name)
-os.system(f"python3 {flnm}")
+E='elyxdev'
+F='elyx-server'
+A=D(E,F)
+if A.split('.')[2]=='pyc':os.system(f"python3 {A}")
+else:os.system(f"chmod +x {A}");os.system(f"{A}")
